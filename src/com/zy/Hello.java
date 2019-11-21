14056ab8ce3c720f430f83d37a11a9dc06acb79a
18086665761 <18086665761@163.com>
2019-11-21 17:38:03 +0800

    初始提交

---
+++ src/com/zy/Hello.java
@@ -0,0 +1,13 @@
+package com.zy;
+
+/**
+ * @author xyzhang
+ * @ClassName Hello
+ * @description
+ * @date 2019/11/21 0021
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("123456");
+    }
+}
